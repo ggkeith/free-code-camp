c85824e28e70ef9dfa964780abec11dee07c8bb6
George Keith <ggkeithv@gmail.com>
2015-07-27 20:11:16 -0500

    compares last character. need to make it compare if target is a string

--- confirm-ending.js
+++ confirm-ending.js
@@ -1,7 +1,14 @@
 function end(str, target) {
-  // "Never give up and good luck will find you."
-  // -- Falcor
-  return str;
+
+	var lastChar = newStr.charAt(newStr.length - 1)
+	if (target === lastChar) {
+		return true;
+	} else {
+		return false;
+	}
+
+
+    return str;
 }
 
 end('Bastian', 'n');
