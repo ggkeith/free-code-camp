2a636e2e6363db82a0b2ee1445ce5cd221faa577
George Keith <ggkeithv@gmail.com>
2015-08-05 00:03:15 -0500

    bonfire slasher flick solution

--- slasher.js
+++ slasher.js
@@ -1,6 +1,13 @@
 function slasher(arr, howMany) {
-  // it doesn't always pay to be first
-  return arr;
+  if (howMany >= arr.length) {
+    return [];
+  } else if (howMany === 0) {
+    return arr;
+  } else {
+    arr.splice(0,howMany);
+    return arr;
+  }
+ 
 }
 
 slasher([1, 2, 3], 2);
