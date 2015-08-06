1aa7632d2e6ea40b831dafa0745cbebc5586739e
George Keith <ggkeithv@gmail.com>
2015-08-05 23:56:02 -0500

    managed to get first test to come back okay, but it is still failing the others

--- mutations.js
+++ mutations.js
@@ -1,5 +1,15 @@
 function mutation(arr) {
-  return arr;
+    var first = arr[0].toLowerCase().split("");
+    var second = arr[1].toLowerCase().split("");
+    
+    if (first,indexOf(second) === 0) {
+        return true;
+    } else if (first.indexOf(second) === -1) {
+        return false;
+    }
+    
+    
+  
 }
 
 mutation(['hello', 'hey']);
