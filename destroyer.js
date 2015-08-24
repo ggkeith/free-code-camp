9c17f25ef1fd317e682f1b70e927cde6ff005b1f
George Keith <ggkeithv@gmail.com>
2015-08-23 23:37:53 -0500

    almost there. stil failing a couple tests

--- destroyer.js
+++ destroyer.js
@@ -1,6 +1,18 @@
 function destroyer(arr) {
-  // Remove all the values
-  return arr;
+  var destroy = [];
+	for (var i = 0; i < arguments.length; i += 1) {
+		destroy.push(arguments[i]);
+	}
+	function listCheck (search) {
+		for (var j = 0; j < destroy.length; j += 1) {
+			if(arr[search] === destroy[j]) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+  return arr.filter(listCheck);
 }
 
 destroyer([1, 2, 3, 1, 2, 3], 2, 3);
