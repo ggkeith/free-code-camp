a317304fde9383eaf030a79a8566cbe68d7a1db4
George Keith <ggkeithv@gmail.com>
2015-09-04 08:22:44 -0500

    solution to Seek and Destroy bonfire

--- destroyer.js
+++ destroyer.js
@@ -1,18 +1,21 @@
 function destroyer(arr) {
-  var destroy = [];
-	for (var i = 0; i < arguments.length; i += 1) {
-		destroy.push(arguments[i]);
-	}
-	function listCheck (search) {
-		for (var j = 0; j < destroy.length; j += 1) {
-			if(arr[search] === destroy[j]) {
-				return true;
-			} else {
-				return false;
-			}
-		}
-	}
-  return arr.filter(listCheck);
+  var elementDestroy = [];
+  
+  for (var i = 0; i < arguments.length; i++) {
+    elementDestroy.push(arguments[i]);
+  }
+  
+  var toKeep = arguments[0].filter(function(element, index) {
+    var toReturn = true;
+    
+    for (var i = 0; i < elementDestroy.length; i++) {
+    if (element === elementDestroy[i]) {
+      toReturn = false;
+      }
+    }
+    return toReturn;
+  });
+  return toKeep;
 }
 
 destroyer([1, 2, 3, 1, 2, 3], 2, 3);
