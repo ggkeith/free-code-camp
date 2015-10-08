b38d99b2bf9fbd2fb0f5a4713e7a7987298d684c
George Keith <ggkeithv@gmail.com>
2015-10-07 19:53:10 -0500

    removed console.log tests from solution

--- pigLatin.js
+++ pigLatin.js
@@ -8,15 +8,10 @@ function translate(str) {
   
   if (str[0] === "a" || str[0] === "e" || str[0] === "i" || str[0] === "o" || str[0] === "u") {
     return str + vowEnd;
-    console.log(newStr);
-    console.log(numVowels);
   } else {
     var removed = str.substr(0, numVowels);
     var shortStr = str.slice(numVowels);
     pigLatin = shortStr.concat(removed) + consEnd;
-    console.log(removed);
-    console.log(shortStr);
-    console.log(pigLatin);
     return pigLatin;
   }
 }
