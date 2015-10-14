7e24dfeca913705f15b780dd3e3408537d323756
George Keith <ggkeithv@gmail.com>
2015-10-14 10:35:31 -0500

    smallest common multiplier bonfire solution

--- smallestCommonMultiple.js
+++ smallestCommonMultiple.js
@@ -1,5 +1,33 @@
 function smallestCommons(arr) {
-  return arr;
+  //sort from smallest to largest
+  arr.sort(function(a, b) {
+    return b - a;
+  });
+
+  // create new array and populate with values
+  var newArr = [];
+  for (var i = arr[0]; i >= arr[1]; i--) {
+    newArr.push(i);
+  }
+
+
+  var num = 0;
+  var newNum = 1;
+  var x;
+
+  // run code while n is not the same as the array length.
+  do {
+    num = newArr[0] * newNum * newArr[1];
+    for (x = 2; x < newArr.length; x++) {
+      if (num % newArr[x] !== 0) {
+        break;
+      }
+    }
+
+    newNum++;
+  } while (x !== newArr.length);
+
+  return num;
 }
 
 
